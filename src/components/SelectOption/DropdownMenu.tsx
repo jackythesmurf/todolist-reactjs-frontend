1419d66df7ca7fa2f341abc6357d5f8ec8363ea9
jackythesmurf <Jackythesmurf@gmail.com>
2024-02-03 21:28:27 +1100

    feature: sort by soonest start or end  date

---
+++ src/components/SelectOption/DropdownMenu.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import OptionItem from './OptionItem'; // Adjust the import path as necessary
+
+interface DropdownMenuProps {
+    options: { label: string; value: string }[];
+    onSelectOption: (value: string) => void;
+}
+
+const DropdownMenu: React.FC<DropdownMenuProps> = ({
+    options,
+    onSelectOption,
+}) => {
+    return (
+        <div className="absolute left-0 right-0 z-10 mt-1 rounded bg-white shadow-lg">
+            {options.map((option) => (
+                <OptionItem
+                    key={option.value}
+                    value={option.value}
+                    label={option.label}
+                    onSelectOption={onSelectOption}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default DropdownMenu;

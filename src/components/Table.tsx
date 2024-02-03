02fb01dc6b33a555b29ea9f8eeb7d11248491e0d
jackythesmurf <Jackythesmurf@gmail.com>
2024-02-03 15:59:06 +1100

    feature: react-query fetches from api displays all tasks

---
+++ src/components/Table.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Tasks from './Tasks';
+
+const Table = () => {
+    return (
+        <div className="m-10 w-[800px] overflow-auto rounded-lg border border-gray-200 bg-gray-50 p-6 shadow-lg">
+            <div className="mb-4">
+                <h1 className="text-center font-mono text-2xl font-semibold text-gray-800">
+                    My Tasks Today
+                </h1>
+            </div>
+            <div className="mb-4 flex items-center justify-center space-x-4"></div>
+            <div className="prose prose-sm max-w-none">
+                <Tasks />
+            </div>
+        </div>
+    );
+};
+
+export default Table;

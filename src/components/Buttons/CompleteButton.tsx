d5a99ae69128125ccb2b168b95743e3114736923
jackythesmurf <Jackythesmurf@gmail.com>
2024-02-04 00:02:14 +1100

    feature: slider for task completion and update cache

---
+++ src/components/Buttons/CompleteButton.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+interface CompleteButtonProps {
+    isComplete: boolean;
+    onToggleComplete: () => void;
+}
+
+const CompleteButton: React.FC<CompleteButtonProps> = ({
+    isComplete,
+    onToggleComplete,
+}) => {
+    return (
+        <label className="ml-3 mt-2.5 flex cursor-pointer items-center">
+            <input
+                type="checkbox"
+                className="hidden"
+                checked={isComplete}
+                onChange={onToggleComplete}
+            />
+            <div className="relative" onChange={onToggleComplete}>
+                <div className="h-4 w-10 rounded-full bg-gray-400 shadow-inner"></div>
+                <div
+                    className={`absolute -left-1 -top-1 h-6 w-6 transform rounded-full bg-white shadow transition-transform ${isComplete ? 'translate-x-full bg-green-500' : 'translate-x-0 bg-gray-400'}`}
+                ></div>
+            </div>
+            <span className="ml-3 text-sm font-medium text-gray-700">
+                {isComplete ? 'Completed' : 'Mark as Complete'}
+            </span>
+        </label>
+    );
+};
+
+export default CompleteButton;
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,54 +1,53 @@
 import React, { useState, useMemo } from 'react';
-import { useGetAllTasks } from '../services/get-all-task';
 import { format } from 'date-fns';
 import { motion } from 'framer-motion';
-import DeleteButton from './Buttons/DeleteButton';
-import EditButton from './Buttons/EditButton';
-import SearchBar from './SearchBar';
 import { useMutation, useQueryClient } from 'react-query';
-import { deleteTask } from '../services/delete-task';
+
 import { Task } from '../types/task';
+import { useGetAllTasks } from '../services/get-all-task';
+import { deleteTask } from '../services/delete-task';
+import { updateTask } from '../services/update-task';
+
+import DeleteButton from './Buttons/DeleteButton';
+import CompleteButton from './Buttons/CompleteButton';
+import SearchBar from './SearchBar';
 import SortButton from './SelectOption/SortButton';
-import { filterTasks } from '../utils/fitlerTasks';
-import { sortTasks } from '../utils/sortTasks';
 import Description from './Description';
+
+import { filterTasks } from '../utils/filterTasks';
+import { sortTasks } from '../utils/sortTasks';
 
 const Tasks: React.FC = () => {
     const queryClient = useQueryClient();
     const { data: allTasks, isLoading, error } = useGetAllTasks();
     const [searchTerm, setSearchTerm] = useState('');
-    const [selectedOption, setSelectedOption] =
-        useState('Sort Options');
+    const [selectedOption, setSelectedOption] = useState('');
 
-    const sortedAndFilteredTasks = useMemo(() => {
-        let filteredTasks = filterTasks(allTasks || [], searchTerm);
-        let sortedTasks = sortTasks(filteredTasks, selectedOption);
-
-        return sortedTasks;
-    }, [searchTerm, allTasks, selectedOption]);
+    const sortedAndFilteredTasks = useMemo(
+        () =>
+            filterTasks(
+                sortTasks(allTasks || [], selectedOption),
+                searchTerm,
+            ),
+        [searchTerm, allTasks, selectedOption],
+    );
 
     const deleteMutation = useMutation(deleteTask, {
-        onSuccess: (deletedTaskId) => {
-            queryClient.setQueryData<Task[]>(
-                'allTasks',
-                (oldTasks) => {
-                    const filteredTasks =
-                        oldTasks?.filter(
-                            (task) => task.id !== deletedTaskId,
-                        ) || [];
-
-                    return filteredTasks;
-                },
-            );
-        },
+        onSuccess: () => queryClient.invalidateQueries('allTasks'),
     });
 
-    const handleDelete = (taskId: string) => {
-        deleteMutation.mutate(taskId);
-    };
+    const completeMutation = useMutation(updateTask, {
+        onSuccess: () => queryClient.invalidateQueries('allTasks'),
+    });
 
-    const handleSortOptionChange = (option: string) => {
-        setSelectedOption(option);
+    const handleDelete = (taskId: string) =>
+        deleteMutation.mutate(taskId);
+
+    const handleComplete = (task: Task) => {
+        completeMutation.mutate({
+            ...task,
+            finished: !task.finished,
+        });
     };
 
     if (isLoading) return <div>Loading tasks...</div>;
@@ -58,54 +57,46 @@ const Tasks: React.FC = () => {
     return (
         <div>
             <div className="flex items-center space-x-6">
-                <div className="flex-grow">
-                    <SearchBar
-                        searchTerm={searchTerm}
-                        setSearchTerm={setSearchTerm}
-                    />
-                </div>
-                <div className="flex-grow">
-                    <SortButton
-                        handleSortOptionChange={
-                            handleSortOptionChange
-                        }
-                    />
-                </div>
+                <SearchBar
+                    searchTerm={searchTerm}
+                    setSearchTerm={setSearchTerm}
+                />
+                <SortButton
+                    handleSortOptionChange={setSelectedOption}
+                />
             </div>
 
             <div className="mx-auto max-w-4xl space-y-6 overflow-auto rounded-lg bg-gray-50 p-6 font-mono text-gray-700">
-                {sortedAndFilteredTasks?.map((task) => (
+                {sortedAndFilteredTasks.map((task) => (
                     <motion.div
+                        key={task.id}
                         initial={{ opacity: 0, y: -10 }}
                         animate={{ opacity: 1, y: 0 }}
                         exit={{ opacity: 0, y: -10 }}
                         transition={{ duration: 0.5 }}
                         className="mb-4 rounded-lg border border-gray-300 bg-white p-4 shadow-sm"
-                        key={task.id}
                     >
                         <div>
-                            <div>
-                                <h1 className="text-xl font-semibold">
-                                    {task.name}
-                                </h1>
-                                <p className="text-sm">
-                                    <strong className="text-blue-500">
-                                        Start:
-                                    </strong>{' '}
-                                    {format(task.startDate, 'Pp')}{' '}
-                                    {' | '}
-                                    <strong className="text-blue-500">
-                                        Due:
-                                    </strong>{' '}
-                                    {format(task.endDate, 'Pp')}
-                                </p>
-
-                                <div>
-                                    <Description
-                                        description={task.description}
-                                    />
-                                </div>
-                            </div>
+                            <h1 className="text-xl font-semibold">
+                                {task.name}
+                            </h1>
+                            <p className="text-sm">
+                                <strong className="text-blue-500">
+                                    Start:
+                                </strong>{' '}
+                                {format(
+                                    new Date(task.startDate),
+                                    'Pp',
+                                )}{' '}
+                                |{' '}
+                                <strong className="text-blue-500">
+                                    Due:
+                                </strong>{' '}
+                                {format(new Date(task.endDate), 'Pp')}
+                            </p>
+                            <Description
+                                description={task.description}
+                            />
                         </div>
                         <div className="mt-2 flex space-x-4">
                             <div
@@ -114,7 +105,14 @@ const Tasks: React.FC = () => {
                                 <DeleteButton />
                             </div>
 
-                            <EditButton />
+                            <div>
+                                <CompleteButton
+                                    isComplete={task.finished}
+                                    onToggleComplete={() =>
+                                        handleComplete(task)
+                                    }
+                                />
+                            </div>
                         </div>
                     </motion.div>
                 ))}

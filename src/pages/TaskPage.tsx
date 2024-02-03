0e36657ec814aa4663a769d49ae6566d6f4c7b70
jackythesmurf <Jackythesmurf@gmail.com>
2024-02-03 19:12:05 +1100

    feature: implement live search with every input

--- src/pages/TaskPage.tsx
+++ src/pages/TaskPage.tsx
@@ -3,6 +3,7 @@ import TaskForm from '../components/TaskForm';
 import Modal from '../components/Model';
 import Table from '../components/Table';
 import AddButton from '../components/Buttons/AddButton';
+import SearchBar from '../components/Buttons/SearchBar';
 
 const TaskPage = () => {
     const [isFormOpen, setIsFormOpen] = useState(false);
@@ -11,13 +12,10 @@ const TaskPage = () => {
 
     return (
         <div>
-            <div className="mb-4 flex items-center justify-center space-x-4">
+            <div className="mt-8 flex items-center justify-center space-x-4">
                 <AddButton toggleForm={toggleForm} />
-                <button>Button</button>
             </div>
-
             <Table />
-
             <Modal isOpen={isFormOpen} onClose={toggleForm}>
                 <TaskForm />
             </Modal>

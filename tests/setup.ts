1ab2eaaf8bf38a74404e962aa542828c7df946df
jackythesmurf <Jackythesmurf@gmail.com>
2024-02-04 14:07:38 +1100

    config: react-testin-library, vitest, jest-dom with typescript setup

---
+++ tests/setup.ts
@@ -0,0 +1,11 @@
+import { afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
+import * as matchers from '@testing-library/jest-dom/matchers';
+import { expect } from 'vitest';
+import '@testing-library/jest-dom/vitest';
+
+expect.extend(matchers);
+
+afterEach(() => {
+    cleanup();
+});
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'; // you have to defined vitest/config to use testing libraries
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    test: {
+        environment: 'jsdom',
+        setupFiles: './tests/setup.ts',
+    },
+});
